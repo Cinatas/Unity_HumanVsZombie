120428296f4302d006124c0d4a8657642f34836c
Cinatas <qd9218@qq.com>
2018-09-10 17:05:22 +0800

    重新构思

---
+++ Assets/test/testPathFind.cs
@@ -0,0 +1,21 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testPathFind : MonoBehaviour {
+
+    Seeker seeker;
+    [SerializeField]
+    public Transform dest;
+	// Use this for initialization
+	void Start () {
+        seeker = GetComponent<Seeker>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        seeker.StartPath(this.transform.position, dest.position);
+	}
+}
